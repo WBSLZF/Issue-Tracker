60d9c724d4175f3e3baba836c43221dbc134452d
luozefei <1439163529@qq.com>
2025-03-14 00:04:56 +0800

    Initializing Project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold ">Hello, Next.js!</h1>
+            <p className="text-base mb-2">This is a simple page.</p>
+        </div>
+    )
+  } 
