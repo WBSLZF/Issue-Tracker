5e04e6a7a3b152e22b8dbdff1732c391ff7d334a
luozefei <1439163529@qq.com>
2025-03-14 19:47:05 +0800

    add transition colors in navbar

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -12,7 +12,7 @@ const NavBar = () => {
         <ul className = "flex space-x-6 pl-5 top-2 h-14 border-b-2 border-b-gray-500 w-full items-center">
             {links.map(link => (
                 <Link 
-                    className='text-zinc-500 hover:text-zinc-800' 
+                    className='text-zinc-500 hover:text-zinc-800 transition-colors' 
                     href={link.href}
                     key={link.href}>{link.label}</Link>))}
         </ul>
