48dd2cf456daee1f978a021539d6142b5123a2ea
luozefei <1439163529@qq.com>
2025-03-14 20:59:56 +0800

    styling the active link

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -1,7 +1,11 @@
+'use client'    
 import Link from 'next/link'
 import React from 'react'
 import { IoMenu } from "react-icons/io5";
+import { usePathname } from 'next/navigation';
+import classnames from "classnames"
 const NavBar = () => {
+  const curPathName = usePathname();
   const links = [
     {"href": "/", "label": <IoMenu />},
     {"href": "/dashboard", "label": "Dashboard"},
@@ -12,7 +16,11 @@ const NavBar = () => {
         <ul className = "flex space-x-6 pl-5 top-2 h-14 border-b-2 border-b-gray-500 w-full items-center">
             {links.map(link => (
                 <Link 
-                    className='text-zinc-500 hover:text-zinc-800 transition-colors' 
+                    className = {classnames({
+                      "text-zinc-900" : curPathName === link.href,
+                      "text-zinc-500": curPathName !== link.href,
+                      "hover:text-zinc-900 transition-colors": true
+                    })}
                     href={link.href}
                     key={link.href}>{link.label}</Link>))}
         </ul>
