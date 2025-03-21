cb19a03525b81c027119ceaf10cc7b84ecd6978b
luozefei <1439163529@qq.com>
2025-03-21 09:35:17 +0800

    add edit button

--- src/app/issues/[id]/page.tsx
+++ src/app/issues/[id]/page.tsx
@@ -1,8 +1,10 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import prisma from "@/app/lib/prisma";
-import { Card, Heading, Text } from "@radix-ui/themes";
+import { Button, Card, Heading, Text } from "@radix-ui/themes";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import Markdown from "react-markdown";
+import { Pencil2Icon } from "@radix-ui/react-icons";
 interface props {
   params: { id: string };
 }
@@ -19,15 +21,23 @@ const IssueDetailPage = async ({ params }: props) => {
 
   if (!issueDetail) notFound();
   return (
-    <div className="grid gap-y-2">
-      <Heading>{issueDetail.title}</Heading>
-      <div className="flex items-center gap-x-4">
-        <IssueStatusBadge status={issueDetail.status}></IssueStatusBadge>
-        <Text>{issueDetail.createdAt.toDateString()}</Text>
+    <div className="grid sm:grid-cols-2 gap-y-2">
+      <div>
+        <Heading>{issueDetail.title}</Heading>
+        <div className="flex items-center gap-x-4">
+          <IssueStatusBadge status={issueDetail.status}></IssueStatusBadge>
+          <Text>{issueDetail.createdAt.toDateString()}</Text>
+        </div>
+        <Card className="max-w-xl prose mt-2">
+          <Markdown>{issueDetail.description}</Markdown>
+        </Card>
       </div>
-      <Card className="max-w-xl prose mt-2">
-        <Markdown>{issueDetail.description}</Markdown>
-      </Card>
+      <div className="mt-2">
+        <Button color="violet">
+          <Pencil2Icon />
+          <Link href={`/issues/${id}/edit`}>Edit Issue</Link>
+        </Button>
+      </div>
     </div>
   );
 };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,31 @@
+import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
-import './globals.css'
-import './theme-config.css'
-import NavBar from './NavBar'
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Inter } from "next/font/google";
+import "./globals.css";
+import NavBar from "./NavBar";
+import "./theme-config.css";
 
 const inter = Inter({
-	subsets: ["latin"],
-	variable: "--font-inter",
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+  preload: true,
 });
 
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en" className={inter.variable}>
-        <body>
-          <Theme accentColor="blue">
-            <NavBar />
-            {children}
-          </Theme>
-        </body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={inter.variable}>
+      <body>
+        <Theme accentColor="blue">
+          <NavBar />
+          {children}
+        </Theme>
+      </body>
+    </html>
+  );
+}
