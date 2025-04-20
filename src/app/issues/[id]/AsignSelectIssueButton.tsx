cc207a92827decb099c59b5ce1f36ba1b456faaa
wbslzf <1439163529@qq.com>
2025-04-20 23:48:23 +0800

    Refactoring Code

--- src/app/issues/[id]/AsignSelectIssueButton.tsx
+++ src/app/issues/[id]/AsignSelectIssueButton.tsx
@@ -7,16 +7,16 @@ import { useQuery } from "@tanstack/react-query";
 import Skeleton from "react-loading-skeleton";
 import toast, { Toaster } from "react-hot-toast";
 const AsignSelectIssueButton = ({ issue }: { issue: Issue }) => {
-  const {
-    data: users,
-    error,
-    isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000, // 数据1分钟更新一次
-    retry: 3,
-  });
+  const handleValueChange = (userId: string) => {
+    axios
+      .patch(`/api/issues/${issue.id}`, {
+        assignedToUserId: userId === "unassigned" ? null : userId,
+      })
+      .catch(() => {
+        toast.error("Failed to assign the issue");
+      });
+  };
+  const { data: users, error, isLoading } = usersQuery;
   if (isLoading) return <Skeleton className="h-8" />;
   if (error) return null;
   return (
@@ -25,15 +25,7 @@ const AsignSelectIssueButton = ({ issue }: { issue: Issue }) => {
         defaultValue={
           issue.assignedToUserId ? issue.assignedToUserId : "unassigned"
         }
-        onValueChange={(userId) => {
-          axios
-            .patch(`/xapi/issues/${issue.id}`, {
-              assignedToUserId: userId === "unassigned" ? null : userId,
-            })
-            .catch(() => {
-              toast.error("Failed to assign the issue");
-            });
-        }}
+        onValueChange={handleValueChange}
       >
         <Select.Trigger placeholder="Assign to" />
         <Select.Content>
@@ -55,4 +47,10 @@ const AsignSelectIssueButton = ({ issue }: { issue: Issue }) => {
   );
 };
 
+const usersQuery = useQuery<User[]>({
+  queryKey: ["users"],
+  queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
+  staleTime: 60 * 1000, // 数据1分钟更新一次
+  retry: 3,
+});
 export default AsignSelectIssueButton;
